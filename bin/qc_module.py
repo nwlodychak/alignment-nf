1c0e76e1bcae2ffebaa2e8304facae8cfd5f09d3
nwlodychak <nwlodychak@gmail.com>
2025-06-03 11:11:30 -0400

    :)

--- bin/qc_module.py
+++ bin/qc_module.py
@@ -1,49 +1,55 @@
-import subprocess
 import argparse
 import os
 import logging
-import time
 from utils import run_command
 
 
-def fastqc(fq1, outdir):
+def get_args():
+    parser = argparse.ArgumentParser(description="""
+
+    DESCRIPTION
+
+        """, formatter_class= argparse.RawTextHelpFormatter)
+    parser.add_argument('--sample_id',
+                    type=str,
+                    default=None,
+                    help='Sample file - e.g SAMPLE001_R1.fastq.gz',
+                    required=True)
+    parser.add_argument('--fastqc_dir',
+                    type=str,
+                    default='.',
+                    help='Path to dir containing sample FASTQs',
+                    required=True)
+    parser.add_argument('--multi_dir',
+                    type=str,
+                    default='.',
+                    help='Path to dir containing sample FASTQs',
+                    required=True)
+    args = parser.parse_args()
+    return args
+
+def fastqc(fq, outdir):
     """
     Generate fastqc files
-    :param fq1: full path for fastq file
+    :param fq: full path for fastq file
     :param outdir: where you want the fastqc files to go
     :return: .html and .zip of fastq
-    """
-    outdir = f'{outdir}/fastqc'
-    os.makedirs(outdir, exist_ok = True)
+    """  
     try:
-        run_command(f'fastqc {fq1} --outdir {outdir}')
+        run_command(f'fastqc {fq} --outdir {outdir}')
     except FileNotFoundError as e:
         logging.error(f"An error occurred: {e}")
         raise
 
 
-def multiqc(outdir):
+def multiqc():
     """
     Generate multiqc files
     :param outdir: results location
     :return: .html and .zip of fastq
     """
-    outdir = f'{outdir}/multiqc'
-    fastqc_indir = f'{outdir}/fastqc'
-    os.makedirs(outdir, exist_ok = True)
     try:
-        run_command(f'multiqc -O {outdir} {fastqc_indir}')
+        run_command(f'multiqc -O {multi_dir} {fastqc_indir}')
     except FileNotFoundError as e:
         logging.error(f"An error occurred: {e}")
         raise
-
-
-def worker(sample_id, read1, read2, outdir):
-# fastqc block
-     try:
-         logging.info(f'FASTQC {sample_id}')
-         fastqc(read1, outdir)
-         fastqc(read2, outdir)
-         time.sleep(5)
-     except Exception as e:
-        logging.error(f"Exception occurred during FASTQC: {e}")
